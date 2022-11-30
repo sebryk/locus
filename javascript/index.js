94e6c0554887952dc478d1deb0f138082fbe650f
Sergei Brykalov <106953297+Sebryk@users.noreply.github.com>
2022-11-30 16:50:40 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.header__menu-icon');
 const menu = document.querySelector('.menu__list');
 const menuBtn = document.querySelector('.menu__btn');
-// const stripe = document.querySelector('.icon__stripe');
+const menuLink = document.querySelectorAll('.menu__link');
 const body = document.body;
 
 burger.addEventListener('click', () => {
@@ -9,4 +9,13 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('icon--active');
   menuBtn.classList.toggle('menu__btn--active');
   body.classList.toggle('body__lock');
+});
+
+menuLink.forEach(function (button) {
+  button.addEventListener('click', () => {
+    body.classList.toggle('body__lock');
+    menu.classList.toggle('menu__active');
+    burger.classList.toggle('icon--active');
+    menuBtn.classList.toggle('menu__btn--active');
+  });
 });
